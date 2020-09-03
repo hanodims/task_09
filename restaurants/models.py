c792689bb6c8719b56a8d534ad5e4ef301320b9c
hanodims <hend--mohammed@outlook.com>
2020-09-03 11:33:50 +0300

    new

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Restaurant(models.Model):
     name = models.CharField(max_length=120)
     description = models.TextField()
@@ -9,3 +10,4 @@ class Restaurant(models.Model):
 
     def __str__(self):
     	return self.name
+
